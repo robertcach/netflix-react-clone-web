504d2e35a964f305b01b84071686ba7cced8e509
Robert Cabrera <cabrera.rch@gmail.com>
2022-04-28 17:08:52 +0200

    change baseURL with env file

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -3,7 +3,7 @@ import { getAccessToken, logout } from '../store/AccessTokenStore'
 
 const createHttp = (useAccessToken = false) => {
   const http = axios.create({
-    baseURL: 'https://robercach-netflix.herokuapp.com',
+    baseURL: `${process.env.REACT_APP_API_HOST_PRODUCTION}`,
   })
 
   http.interceptors.request.use((request) => {
